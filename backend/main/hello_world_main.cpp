fbd0188bb9b41dff4d687e06e02001f5a0a731f0
Malachi Habib <Malachi.Habib@NQRY.com.au>
2024-11-04 10:29:52 +1030

    align dependencies

---
+++ backend/main/hello_world_main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "dht.h"
+
+#define DHT22_GPIO GPIO_NUM_4
+
+extern "C" void app_main()
+{
+    while (1)
+    {
+        float temperature = 0.0f, humidity = 0.0f;
+        esp_err_t res = dht_read_float_data(DHT_TYPE_AM2301, DHT22_GPIO, &humidity, &temperature);
+        if (res == ESP_OK)
+        {
+            printf("Temperature: %.1f°C, Humidity: %.1f%%\n", temperature, humidity);
+        }
+        else
+        {
+            printf("Could not read data from sensor, error: %d\n", res);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
